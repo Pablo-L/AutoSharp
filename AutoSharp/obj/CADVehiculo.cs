d4c219df7aad8a879af87960545d16fd4cd8be92
Alejandro <avrs1@alu.ua.es>
2019-04-12 10:29:14 +0200

    Added CADVechiulo class

---
+++ AutoSharp/obj/CADVehiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace library
+{
+	public CADVehiculo()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
