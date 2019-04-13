6692362b32db479d8fcd914120fe259d084c79be
Regueman <daniel.regueira.ayuso@gmail.com>
2019-04-13 12:20:30 +0200

    49232451P - CADOferta y CADCita

---
+++ AutoSharp/CADCita.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AutoSharp
+{
+	public class CADCita
+	{
+
+        private string constring;
+        public string Constring { get => constring; set => constring = value; }
+
+
+        public CADCita() { }
+
+
+        public bool importCADCita(ENCita enCita) { }
+
+        public bool insertCADCita(ENCita enCita) { }
+
+        public bool updateCADCita(ENCita enCita) { }
+
+        public bool delete(ENCita enCita) { }
+
+        //public void importNextCADCita(ENCita enCita) { }
+
+        //public void importPrevCADCita(ENCita enCita) { }
+
+    }
+}
---
+++ AutoSharp/CADOferta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AutoSharp
+{
+    public class CADOferta
+    {
+
+        private string constring;
+        public string Constring { get => constring; set => constring = value; }
+
+
+        public CADOferta() { }
+
+
+        public bool importCADOferta(ENOferta enOferta) { }
+
+        public bool insertCADOferta(ENOferta enOferta) { }
+
+        public bool updateCADOferta(ENOferta enOferta) { }
+
+        public bool deleteCADOferta(ENOferta enOferta) { }
+
+        //public void importNextCADOferta(ENCita enOferta) { }
+
+        //public void importPrevCADOferta(ENCita enOferta) { }
+
+    }
+}
