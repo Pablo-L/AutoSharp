7a5610d77f485f8dca9b4c62b42e26981f785969
Alejandro <avrs1@alu.ua.es>
2019-04-13 10:54:16 +0200

    Added methods to CADVehiculo class

--- AutoSharp/obj/CADVehiculo.cs
+++ AutoSharp/obj/CADVehiculo.cs
@@ -12,10 +12,56 @@ using System.Data.SqlTypes;
 /// </summary>
 namespace library
 {
-	public CADVehiculo()
+	public class CADVehiculo
 	{
-		//
-		// TODO: Add constructor logic here
-		//
+        /// <summary>
+        /// Cadena de conexión de la base de datos
+        /// </summary>
+        /// <returns></returns>
+        private string constring();
+        /// <summary>
+        /// Constructor por defecto
+        /// </summary>
+        public CADVehiculo()
+        {
+            //constring = ConfigurationManager.ConnectionStrings["DataBaseConnection"].ToString();
+        }
+        /// <summary>
+        /// Crea el vehículo indicado en la bd
+        /// </summary>
+        /// <param name="en">atributos del vehículo a crear</param>
+        /// <returns></returns>
+        public bool createVehiculo(ENVehiculo en)
+        {
+
+        }
+        /// <summary>
+        /// Lee el vehículo indicado de la base de datos
+        /// </summary>
+        /// <param name="en">vehículo a leer</param>
+        /// <returns></returns>
+        public bool readVehiculo(ENVehiculo en)
+        {
+
+        }
+        /// <summary>
+        /// Borra el vehículo indicado de la base de datos
+        /// </summary>
+        /// <param name="en">vehículo a borrar</param>
+        /// <returns></returns>
+        public bool deleteVehiculo(ENVehiculo en)
+        {
+
+        }
+        /// <summary>
+        /// Actualiza el vehículo indicado en la base de datos
+        /// </summary>
+        /// <param name="en"></param>
+        /// <returns></returns>
+        public bool updateVehiculo(ENVehiculo en)
+        {
+
+        }
+        
 	}
 }
