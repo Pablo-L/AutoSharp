f950a97ec0cb8d8ecd6f502c6582bac5b7a686a7
Pablo <pld7@alu.ua.es>
2019-04-13 17:28:26 +0200

    Añadido Particular y Reserva

---
+++ Libraryt/CADParticular.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class CADParticular
+    {
+        public CADParticular()
+        {
+
+        }
+
+        public void createParticular()
+        {
+
+        }
+
+        public void updateParticular()
+        {
+
+        }
+
+        public void deleteParticular()
+        {
+
+        }
+
+        public bool readParticular()
+        {
+            return true;
+        }
+    }
+}
---
+++ Libraryt/CADReserva.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class CADReserva
+    {
+        public CADReserva()
+        {
+
+        }
+        public void createReserva()
+        {
+
+        }
+
+        public void updateReserva()
+        {
+
+        }
+
+        public void deleteReserva()
+        {
+
+        }
+
+        public bool readReservation()
+        {
+            return true;
+        }
+    }
+}
