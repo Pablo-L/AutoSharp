57d99dd3d063b9978555a5a72f80bbb9d763817e
Pablo <pld7@alu.ua.es>
2019-04-13 17:38:13 +0200

    Añadido Cita y Oferta

---
+++ AutoSharp/ENOferta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace library
+{
+    public class ENOferta
+    {
+        private string id;
+
+        private DateTime inicioOferta;
+
+        private DateTime finOferta;
+
+        private int descuento;
+
+
+        public bool createENOferta() { }
+
+        public bool readENOferta() { }
+
+        public bool updateENOferta() { }
+
+        public bool deleteENOferta() { }
+    }
+}
