23ac1c9b3b1d527fb70b9d64a0fe5155b6885b46
LambertoRM <lrm76@mscloud.ua.es>
2019-04-14 02:10:00 +0200

    Creadas las clases: "CADCategoria", "ENCategoria", "CADListaFavoritos", "ENListaFavoritos".

---
+++ CADListaFavoritos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+/// <summary>
+/// CADListaFavoritos
+/// </summary>
+public class CADListaFavoritos
+{
+    /// <summary>
+    /// Cadena de conexión a la BBDD.
+    /// </summary>
+    private string constring;
+
+    /// <summary>
+    /// Constructor por defecto
+    /// </summary>
+	public CADListaFavoritos()
+    {
+        //constring = ConfigurationManager.ConnectionStrings["DataBaseConnection"].ToString();
+    }
+
+    /// <summary>
+    /// Crea una lista de favoritos
+    /// </summary>
+    /// <param name="en"></param>
+    /// <returns></returns>
+    public bool createListaFav(ENListaFavoritos en)
+    {
+
+    }
+
+    public bool readListaFav(ENListaFavoritos en)
+    {
+
+    }
+
+    public bool updateListaFav(ENListaFavoritos en)
+    {
+
+    }
+
+    public bool
+}
---
+++ ENListaFavoritos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+public class ENListaFavoritos
+{
+    public ENListaFavoritos()
+    {
+    }
+
+
+}
