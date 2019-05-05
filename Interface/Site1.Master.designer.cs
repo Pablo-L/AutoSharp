f213f8d1c64d0760ae92f62187a73059dc7e44c2
Almilkar Cruz Orias <aco32@alu.ua.es>
2019-05-05 20:57:22 +0200

    Site1.Master.designer.cs moddified

--- Interface/Site1.Master.designer.cs
+++ Interface/Site1.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Interface {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
         /// Control ImagenesScroll.
         /// </summary>
         /// <remarks>
@@ -29,5 +38,14 @@ namespace Interface {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ImagenesScroll;
+        
+        /// <summary>
+        /// Control login_page.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder login_page;
     }
 }
