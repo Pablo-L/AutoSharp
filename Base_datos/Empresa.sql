482badf1d5237092029c77ffd3074d1f3e9dba96
Pablo <pld7@alu.ua.es>
2019-05-05 15:49:32 +0200

    Añadidad la base de datos (Primera versión)

---
+++ Base_datos/Empresa.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Empresa
+(
+	[cif] VARCHAR(9) NOT NULL PRIMARY KEY, 
+    [nombre] VARCHAR(15) NULL, 
+    [email] VARCHAR(35) NULL, 
+    [telefono] INT NULL, 
+    [direccion] VARCHAR(15) NULL, 
+    [tamanyo] NCHAR(10) NULL, 
+    [contrasenya] VARCHAR(20) NOT NULL
+)
---
+++ Base_datos/Particulares.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE Particular
+(
+	[nif] VARCHAR(9) NOT NULL PRIMARY KEY, 
+    [nombre] VARCHAR(15) NULL, 
+    [apellidos] VARCHAR(30) NULL, 
+    [email] VARCHAR(35) NULL, 
+    [telefono] INT NULL, 
+    [direccion] VARCHAR(20) NULL, 
+    [iban] VARCHAR(24) NULL, 
+    [contraseña] VARCHAR(10) NOT NULL
+)
