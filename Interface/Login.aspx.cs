a1aed422f04cac812ed74586d7ed75d0547c222d
Almilkar Cruz Orias <aco32@alu.ua.es>
2019-05-05 20:59:29 +0200

    Added Login.aspx.cs

---
+++ Interface/Login.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Interface
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void log_in_button(object sender, EventArgs e)
+        {
+
+        }
+        protected void log_in_button_part(object sender, EventArgs e)
+        {
+
+        }
+        protected void log_in_button_empre(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
