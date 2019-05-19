d71123f91a3a9d91f09eb6eabe74c6df67a2123d
Almilkar Cruz Orias <aco32@alu.ua.es>
2019-05-20 00:46:29 +0200

    Added Library folder

---
+++ Library/ENCompra.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// ENCompra representa la compra de un vehículo. Esta clase, contendrá el cif de la empresa vendedora,
+/// el dni del comprador particular, la matricula del vehículo vendo y la fecha de la transacción.
+/// </summary>
+namespace Library
+{
+    /// <summary>
+    /// Clase ENCompra
+    /// </summary>
+    public class ENCompra
+    {
+        /// <summary>
+        /// Atributo privado comprado (nif)
+        /// </summary>
+        private string comprador;
+
+        /// <summary>
+        /// Atributo privado vendedor (cif)
+        /// </summary>
+        private string vendedor;
+
+        /// <summary>
+        /// Atributo privado vehículo (matricula)
+        /// </summary>
+        private string vehiculo;
+
+        /// <summary>
+        /// Atributo privado fecha
+        /// </summary>
+        private DateTime fecha;
+
+        /// <summary>
+        /// Propiedad pública compradorCompra
+        /// </summary>
+        public string compradorCompra
+        {
+            get { return comprador; }
+            set { comprador = value; }
+        }
+
+        /// <summary>
+        /// Propiedad pública vendedorCompra
+        /// </summary>
+        public string vendedorCompra
+        {
+            get { return vendedor; }
+            set { vendedor = value; }
+        }
+
+        /// <summary>
+        /// Propiedad pública vehiculoCompra
+        /// </summary>
+        public string vehiculoCompra
+        {
+            get { return vehiculo; }
+            set { vehiculo = value; }
+        }
+
+        /// <summary>
+        /// Propiedad pública fechaCompra
+        /// </summary>
+        public DateTime fechaCompra
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+        /// <summary>
+        /// Constructor por defecto
+        /// </summary>
+        public ENCompra()
+        {
+            this.compradorCompra = null;
+            this.vendedorCompra = null;
+            this.vehiculoCompra = null;
+            this.fechaCompra = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Constructor por valor
+        /// </summary>
+        /// <param name="comprador">Parámetro string comprador</param>
+        /// <param name="vendedor">Parámetro string vendedor</param>
+        /// <param name="vehiculo">Parámetro string vehiculo</param>
+        /// <param name="fecha">Parámetro DateTime fecha</param>
+        public ENCompra(string comprador, string vendedor, string vehiculo, DateTime fecha)
+        {
+            this.compradorCompra = comprador;
+            this.vendedorCompra = vendedor;
+            this.vehiculoCompra = vehiculo;
+            this.fechaCompra = fecha;
+        }
+
+        /// <summary>
+        /// Crea la compra indicada en la base de datos
+        /// </summary>
+        /// <returns>Devuelve true si la operación se realiza correctamente</returns>
+        public bool createCompra()
+        {
+            CADCompra cadc = new CADCompra();
+            return cadc.createCompra(this);
+        }
+
+        /// <summary>
+        /// Lee la compra indicada dela base de datos
+        /// </summary>
+        /// <returns>Devuelve true si la operación se realiza correctamente</returns>
+        public bool readCompra()
+        {
+            CADCompra cadc = new CADCompra();
+            return cadc.readCompra(this);
+        }
+
+        /// <summary>
+        /// Borra la compra indicada de la base de datos
+        /// </summary>
+        /// <returns>Devuelve true si la operación se realiza correctamente</returns>
+        public bool deleteCompra()
+        {
+            CADCompra cadc = new CADCompra();
+            return cadc.deleteCompra(this);
+        }
+
+        /// <summary>
+        /// Actualiza la compra indicada de la base de datos
+        /// </summary>
+        /// <returns>Devuelve true si la operación se realiza correctamente</returns>
+        public bool updateCompra()
+        {
+            CADCompra cadc = new CADCompra();
+            return cadc.updateCompra(this);
+        }
+    }
+}
---
+++ Library/ENConsulta.cs
@@ -0,0 +1,118 @@
+
+using System;
+
+namespace Library
+{
+
+    /// <summary>
+    /// ENConsulta
+    /// </summary>
+    public class ENConsulta
+    {
+        /// <summary>
+        /// atributo privado fecha
+        /// </summary>
+        private DateTime fecha;
+
+        /// <summary>
+        /// propiedad publica fecha_
+        /// </summary>
+        public DateTime fecha_ { get => fecha; set => fecha = value; }
+
+        /// <summary>
+        /// atrivbuto privado id
+        /// </summary>
+        private int id;
+
+        /// <summary>
+        /// propiedad publica id_
+        /// </summary>
+        public int id_ { get => id; set => id = value; }
+
+        /// <summary>
+        /// atributo privado pregunta
+        /// </summary>
+        private string pregunta;
+
+        /// <summary>
+        /// propiedad publica pregunta_
+        /// </summary>
+        public string pregunta_ { get => pregunta; set => pregunta = value; }
+
+        /// <summary>
+        /// atributo privado respuesta
+        /// </summary>
+        private string respuesta;
+
+        /// <summary>
+        /// propiedad publica respuesta_
+        /// </summary>
+        public string respuesta_ { get => respuesta; set => respuesta = value; }
+
+        /// <summary>
+        /// constructor por defecto
+        /// </summary>
+        public ENConsulta()
+        {
+            this.fecha_ = DateTime.Now;
+            this.id_ = 0;
+            this.pregunta_ = null;
+            this.respuesta_ = null;
+        }
+
+        /// <summary>
+        /// constructor por valores
+        /// </summary>
+        /// <param name="fecha_"> parametro datetime fecha </param>
+        /// <param name="id_"> parametro int id </param>
+        /// <param name="pregunta_"> parametro string pregunta </param>
+        /// <param name="respuesta"> parametro string respuesta </param>
+        public ENConsulta(DateTime fecha_, int id_, string pregunta_, string respuesta)
+        {
+            this.fecha_ = fecha_;
+            this.id_ = id_;
+            this.pregunta_ = pregunta_;
+            this.respuesta_ = respuesta_;
+        }
+
+        /// <summary>
+        /// crear una consulta en la bbdd
+        /// </summary>
+        /// <returns>Devuelve true si la operación se realiza correctamente</returns>
+        public bool createConsulta()
+        {
+            CADConsulta en = new CADConsulta();
+            return en.createConsulta(this);
+        }
+
+        /// <summary>
+        /// lee una consulta en la bbdd
+        /// </summary>
+        /// <returns>Devuelve true si la operación se realiza correctamente</returns>
+        public bool readConsulta()
+        {
+            CADConsulta en = new CADConsulta();
+            return en.readConsulta(this);
+        }
+
+        /// <summary>
+        /// actualiza una consulta en la bbdd
+        /// </summary>
+        /// <returns>Devuelve true si la operación se realiza correctamente</returns>
+        public bool updateConsulta()
+        {
+            CADConsulta en = new CADConsulta();
+            return en.updateConsulta(this);
+        }
+
+        /// <summary>
+        /// borra una consulta de la bbdd
+        /// </summary>
+        /// <returns>Devuelve true si la operación se realiza correctamente</returns>
+        public bool deleteConsulta()
+        {
+            CADConsulta en = new CADConsulta();
+            return en.deleteConsulta(this);
+        }
+    }
+}
