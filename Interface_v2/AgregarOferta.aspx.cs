fe9508aa4e36497640a7ef60ae12be4aee8a7f6c
Almilkar Cruz Orias <aco32@alu.ua.es>
2019-05-26 23:26:41 +0200

    Added AgregarOferta.aspx

---
+++ Interface_v2/AgregarOferta.aspx.cs
@@ -0,0 +1,21 @@
+﻿using Library;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Interface_v2.imagenes
+{
+    public partial class AgregarOferta : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
