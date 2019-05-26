2b22943ec68b91ef4d1033587e6ec013369d1988
Almilkar Cruz Orias <aco32@alu.ua.es>
2019-05-26 23:30:01 +0200

    Added CitaCLiente

---
+++ Interface_v2/CitaCiliente.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Interface_v2
+{
+    public partial class CitaCliente : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["nif"] == null)
+            {
+                Response.Redirect("~/Inicio.aspx");
+            }
+        }
+    }
+}
